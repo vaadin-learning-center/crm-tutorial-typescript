abc7c6c1091de89d42bee6fab9d316bfe8453438
Viktor Lukashov <viktor@vaadin.com>
2020-11-30 22:53:18 +0200

    [WIP] feat: normalize the Contact and Company data in the store (naïve approach)
    This allows keeping the company name consistent between Employee and Company lists when making optimistic updates.
    
    When a company is modified in the company list view, both the dashboard and the contact list became out-of-sync with the company list. This was due to data duplication between the Company and the Contact lists in the entity store. A contact entity in the Contacts list references a company entity. This was a different JS instance than the one stored in the Company domain store.
    
    This commit adds the code to ensure there is only one instance for each entity with a given ID in the entity store.

--- frontend/stores/entity-store.ts
+++ frontend/stores/entity-store.ts
@@ -14,8 +14,10 @@ import {
   saveCompany,
 } from '../generated/CompanyEndpoint';
 import Company from '../generated/com/vaadin/tutorial/crm/backend/entity/Company';
+import CompanyModel from '../generated/com/vaadin/tutorial/crm/backend/entity/CompanyModel';
 import State from '../generated/com/vaadin/tutorial/crm/backend/entity/Company/State';
 import Contact from '../generated/com/vaadin/tutorial/crm/backend/entity/Contact';
+import ContactModel from '../generated/com/vaadin/tutorial/crm/backend/entity/ContactModel';
 import Status from '../generated/com/vaadin/tutorial/crm/backend/entity/Contact/Status';
 
 export class EntityStore {
@@ -43,7 +45,54 @@ export class EntityStore {
 
   private async updateContacts() {
     const contacts = await find('');
-    runInAction(() => this.contacts = contacts);
+    runInAction(() => {
+      // Create an id -> Contact map with all currently stored contacts
+      const contactById = {} as {[k in string]: Contact};
+      this.contacts.forEach(c => contactById[c.id] = c);
+
+      // Iterate over the received contacts list and for all contacts that
+      // are already currently stored (equal by id), replace the new JS
+      // instances with the old JS instances, and then update all the
+      // properties in the old JS instances to match the new values.
+      //
+      // This is done to keep direct JS references working (e.g. companies
+      // in the Company domain store keep references to Contact objects
+      // from this store. If the JS objects change, the Company store
+      // becomes inconsistent with the Contact store).
+      contacts.forEach((c, i) => {
+        const stored = contactById[c.id];
+        if (stored) {
+          stored.firstName = c.firstName;
+          stored.lastName = c.lastName;
+          stored.email = c.email;
+          stored.status = c.status;
+          stored.company = c.company
+            ? this.resolveCompany(c.company.id, c.company)
+            : c.company;
+
+          contacts[i] = stored;
+        }
+      });
+
+      // replace the array object itself
+      this.contacts = contacts;
+    });
+  }
+
+  private resolveContact(id: Contact['id'], update: Partial<Contact>): Contact {
+    const stored = this.contacts.find(c => c.id == id);
+    if (stored) {
+      Object.assign(stored, update); // mutate the stored object in place
+      return stored;
+    } else {
+      const contact = {
+        ...ContactModel.createEmptyValue(),
+        ...update,
+        id
+      };
+      this.contacts.push(contact);
+      return contact;
+    }
   }
 
   async saveContact(contact: Contact) {
@@ -74,7 +123,53 @@ export class EntityStore {
 
   private async updateCompanies() {
     const companies = await findAllCompanies();
-    runInAction(() => this.companies = companies);
+    runInAction(() => {
+      // Create an 'id -> Company' map with all currently stored companies
+      const companyById = {} as {[k in string]: Company};
+      this.companies.forEach(c => companyById[c.id] = c);
+
+      // Iterate over the received companies list and for all companies that
+      // are already currently stored (equal by id), replace the new JS
+      // instances with the old JS instances, and then update all the
+      // properties in the old JS instances to match the new values.
+      //
+      // This is done to keep direct JS references working (e.g. contacts
+      // in the Contact domain store keep references to Company objects
+      // from this store. If the JS objects change, the Contact store
+      // becomes inconsistent with the Company store).
+      companies.forEach((c, i) => {
+        const stored = companyById[c.id];
+        if (stored) {
+          stored.name = c.name;
+          stored.description = c.description;
+          stored.state = c.state;
+          stored.employees = c.employees
+            ? c.employees.map(e => this.resolveContact(e.id, e))
+            : c.employees;
+
+          companies[i] = stored;
+        }
+      });
+
+      // replace the array object itself
+      this.companies = companies;
+    });
+  }
+
+  resolveCompany(id: Company['id'], update: Partial<Company>): Company {
+    const stored = this.companies.find(c => c.id == id);
+    if (stored) {
+      Object.assign(stored, update); // mutate the stored object in place
+      return stored;
+    } else {
+      const company = {
+        ...CompanyModel.createEmptyValue(),
+        ...update,
+        id
+      };
+      this.companies.push(company);
+      return company;
+    }
   }
 
   async saveCompany(company: Company) {
